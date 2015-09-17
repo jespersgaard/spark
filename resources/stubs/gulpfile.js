e2116e2bc462fc5f6f5a84630bf93237fb357b25
Marcel Pociot <m.pociot@gmail.com>
2015-09-17 11:09:02 +0200

    Fixed gulp task to add the VAT Calculator JS

--- resources/stubs/gulpfile.js
+++ resources/stubs/gulpfile.js
@@ -13,10 +13,6 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
-        .scripts([
-            'vat_calculator.js',
-            'app.js'
-        ], 'public/js/app.js')
-       .browserify('app.js')
+       .browserify(['app.js', 'vat_calculator.js'], 'public/js/app.js')
        .version(['css/app.css', 'js/app.js']);
 });
